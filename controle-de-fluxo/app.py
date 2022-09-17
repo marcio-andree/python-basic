1fb56e49b0761908fe602f32c1123d12470f146a
Marcio André <andremarcio1988@gmail.com>
2022-09-17 20:57:06 -0300

    controle de fluxo

---
+++ controle-de-fluxo/app.py
@@ -0,0 +1,22 @@
+# Operadores lógicos
+# Operadores boleanos
+# Operadores de igualdade
+# Operadores ternário
+idade = 20
+print(idade > 30)
+print(idade < 30)
+print(idade <= 30)
+print(idade >= 30)
+print(idade == 30)
+print(idade  !=30)
+
+
+
+# pessoa1 = str(input('Digite seu nome:'))
+# idade1 = int(input('Digite sua idade: '))
+# pessoa2 = str(input('Digite seu nome: '))
+# idade2 = int(input('Digite sua idade: '))
+# if idade1 > idade2:
+#   print(f'{pessoa1}: é mais velho(ª) que {pessoa2}')
+# else:
+#   print(f'{pessoa2}: é mais velha que {pessoa1}')
