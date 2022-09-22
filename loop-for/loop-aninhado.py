f74a08817c2b1f4151a2f25b763e306c3c436c01
Marcio André <andremarcio1988@gmail.com>
2022-09-22 09:38:15 -0300

    loop aninhado

---
+++ loop-for/loop-aninhado.py
@@ -0,0 +1,11 @@
+paises = ['Canada', 'Nova Zelandia', 'Alemanha', 'França']
+estacoes = ['primavera', 'verão', 'outono', 'inverno']
+for pais in paises:
+  for estacao in estacoes:
+    print(f'{pais} - {estacao}')
+
+celulares = ['Asus', 'Samsung', 'Sony', 'Iphone']
+versoes = ['plus', 'premium plus', 'premium deluxe', 'plus premium ultra']
+for celular in celulares:
+  for versao in versoes:
+    print(f'{celular} --> {versao}')
