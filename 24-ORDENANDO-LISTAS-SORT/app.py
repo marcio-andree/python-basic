f9d934a6656804d93cc71ef2c9be26fa62df9b42
Marcio André <andremarcio1988@gmail.com>
2022-11-11 14:46:05 -0300

    ordenando arrays método sort e reverse

---
+++ 24-ORDENANDO-LISTAS-SORT/app.py
@@ -0,0 +1,13 @@
+valores = [31,23,37,7,20,23,31,23,6,36,21]
+# metodo sort() ordena uma lista em ordem crescente
+valores.sort()
+print(valores)
+
+
+# metodo sort(reverse=True) organiza uma lista em ordem decrescente
+valores.sort(reverse=True)
+print(valores)
+
+
+valores.reverse()
+print(valores)
