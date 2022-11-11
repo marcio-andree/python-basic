ec8bf0182a93c25a388eb0f2178bf8d6c6e5e622
Marcio André <andremarcio1988@gmail.com>
2022-11-11 16:06:25 -0300

    tuplas

---
+++ 27-TUPLA/app.py
@@ -0,0 +1,10 @@
+# tuplas armazena valores que não devem ser alterados depois que são criados
+
+ataqueFla = ('Pedro', 'Gabi','Bruno','Cebolinha')
+meioFla = ('Thiago Maia', 'João Gomes', 'Gerson', 'Vidal')
+print(ataqueFla)
+print(ataqueFla[1])
+
+frontFla = ataqueFla + meioFla
+
+print(frontFla)
